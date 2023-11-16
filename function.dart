5139f29ab6e651688bc114dcdb34aebbb74e9d99
abirh <abirhossain80760@gmail.com>
2023-11-16 22:05:00 +0600

    AAAA

---
+++ function.dart
@@ -0,0 +1,19 @@
+
+void main(){
+  var number =[10,20,30,40,50,60,70,80,90];
+  String value='dhaka,rangpur,barishal,khulna,cumilla';
+
+print( '${value.toString()}'+'${number}');
+
+
+number.removeLast();
+
+  print( '${value.toString()}'+'${number}');
+
+//IJHOTJHHOJO
+
+
+
+
+
+}
